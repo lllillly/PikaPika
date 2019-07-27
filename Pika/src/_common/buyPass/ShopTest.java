165010162845ca68fffb95b503dbf946b023fc05
user <user@210.123.201.13>
2019-07-27 23:34:34 +0900

    buyPass 수정본	third commit

--- Pika/src/_common/buyPass/ShopTest.java
+++ Pika/src/_common/buyPass/ShopTest.java
@@ -1,5 +1,7 @@
 package _common.buyPass;
 
+import java.util.Scanner;
+
 public class ShopTest {
 	public static void main(String[] args) {
 
@@ -9,9 +11,16 @@ public class ShopTest {
 		// 현재 상황을 출력해주는 current메소드
 		// buy 메소드를 통해 product 입력 및 출력
 
+		System.out.println("비밀번호를 입력하세요	>>	");
+		// 비밀번호 7777 입력 시 로그인 성공
+		Scanner sc = new Scanner(System.in);
+		
+		String pass = sc.nextLine();
+		
+		
 		Shop s1 = new Shop("Test마트");
 
-		System.out.println(s1.login("admin", "7777"));
+		System.out.println(s1.login("admin", pass));
 		s1.current();
 		s1.buy("사과");
 		s1.buy("복숭아");
