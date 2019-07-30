eac77f6873e3fd238280cb03acca68402b7bb039
DEV_YUN <riosee2415@gmail.com>
2019-07-30 22:16:59 +0900

    COMMIT;

---
+++ Pika/src/model/MemberVO.java
@@ -0,0 +1,114 @@
+package model;
+
+/**
+ * @author sanghoyun
+ *
+ */
+public class MemberVO {
+	
+	private String uid;
+	private String cyberBank;
+	private String membershipLevel;
+	private String siteViews;
+	private String id;
+	private String password;
+	private String name;
+	private String nickname;
+	private String add1;
+	private String add2;
+	private String point;
+	private String mobile;
+	private String email;
+	private String coupon;
+	
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getCyberBank() {
+		return cyberBank;
+	}
+	public void setCyberBank(String cyberBank) {
+		this.cyberBank = cyberBank;
+	}
+	public String getMembershipLevel() {
+		return membershipLevel;
+	}
+	public void setMembershipLevel(String membershipLevel) {
+		this.membershipLevel = membershipLevel;
+	}
+	public String getSiteViews() {
+		return siteViews;
+	}
+	public void setSiteViews(String siteViews) {
+		this.siteViews = siteViews;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getAdd1() {
+		return add1;
+	}
+	public void setAdd1(String add1) {
+		this.add1 = add1;
+	}
+	public String getAdd2() {
+		return add2;
+	}
+	public void setAdd2(String add2) {
+		this.add2 = add2;
+	}
+	public String getPoint() {
+		return point;
+	}
+	public void setPoint(String point) {
+		this.point = point;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCoupon() {
+		return coupon;
+	}
+	public void setCoupon(String coupon) {
+		this.coupon = coupon;
+	}
+	
+	
+	
+	
+	
+	
+}
