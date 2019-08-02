4c310f25cc9618990d03b0dc9905de3c2d0900e4
user <user@DESKTOP-NUTM8UL>
2019-08-03 03:19:34 +0900

    Login method test

---
+++ Pika/src/signup/DBconnection2.java
@@ -0,0 +1,32 @@
+package signup;
+
+import java.util.Scanner;
+
+import scott_emp.dao.DBconnection;
+
+public class DBconnection2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("웹페이지 ");
+	
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("ID : ");
+		String data1 = sc.nextLine();
+		
+		System.out.println();
+		
+		System.out.print("PASS : ");
+		String data2 = sc.nextLine();
+		
+		DBconnection db = new DBconnection(data1, data2);
+		
+	
+		
+		
+	}
+	
+	
+	
+}
