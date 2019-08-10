81a29e1ea99f0cf875a9fd367d454f19b541373e
user <user@DESKTOP-NUTM8UL>
2019-08-10 15:25:22 +0900

    위치 옮김

--- Pika/src/signup/Signup_crud_controller.java
+++ Pika/src/signup/Signup_crud_controller.java
@@ -1,5 +1,29 @@
 package signup;
 
-public class Signup_crud_controller {
+import java.util.Scanner;
 
+import scott_emp.dao.DBconnection;
+
+public class Signup_crud_controller {
+public static void main(String[] args) {
+		
+		System.out.println("웹페이지 ");
+	
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("ID : ");
+		String data1 = sc.nextLine();
+		
+		System.out.println();
+		
+		System.out.print("PASS : ");
+		String data2 = sc.nextLine();
+		
+		DBconnection db = new DBconnection(data1, data2);
+		
+	
+		
+		
+	}
+	
 }
